1bfb255623ebfaa6b51ceeaa41eed6b9adbd9364
Thomas <65050898+tvonment@users.noreply.github.com>
2021-02-10 09:29:33 +0000

    change Envslot to Environment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from: ' + process.env.ENVSLOT)
+  res.send('Hello from: ' + process.env.Environment)
 })
 
 app.listen(port, () => {
