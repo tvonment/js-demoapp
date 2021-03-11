54dad850387753ed5a57acf7596c27fe081ae89b
Thomas <65050898+tvonment@users.noreply.github.com>
2021-03-11 09:55:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Liebe Mobiliar')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
