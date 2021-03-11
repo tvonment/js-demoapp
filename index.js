c51c5e88ed2f10c49591cd79b1973c0598738409
Thomas <65050898+tvonment@users.noreply.github.com>
2021-03-11 11:36:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Liebe Mobiliar! Ich habe leider die ' + process.env.Environment + 's Environment kaputt gemacht.')
 })
 
 app.listen(port, () => {
