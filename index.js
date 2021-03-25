93c32760789840df542fe5b20859d3b6e7ed27a5
Thomas <65050898+tvonment@users.noreply.github.com>
2021-03-25 09:40:11 +0000

    add envs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.port || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello ' + process.env.Name + ' - You ar running on: ' + process.env.Env)
 })
 
 app.listen(port, () => {
