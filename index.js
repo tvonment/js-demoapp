7ba71d1571e3c4302ee50297874d02e31b9bf3c1
Thomas <65050898+tvonment@users.noreply.github.com>
2022-02-23 08:29:02 +0000

    clear

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 80;
-const setting = process.env.NAME || "BOB";
+//const setting = process.env.NAME || "BOB";
 
 app.get('/', (req, res) => {
-  res.send('Hello World!' + ' Name: ' + setting)
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
