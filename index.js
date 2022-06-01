a8ca8c60063691fe9f4178578e746d0547891578
Thomas <65050898+tvonment@users.noreply.github.com>
2022-06-01 13:43:36 +0000

    add authData

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80;
 
+let authData = fetch(".auth/me");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(authData)
 })
 
 app.listen(port, () => {
