2318b0e456d4231c0b39a583718181df3219463c
Thomas <65050898+tvonment@users.noreply.github.com>
2022-06-01 13:54:57 +0000

    try 2

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80;
 
-let authData = fetch(".auth/me");
 
 app.get('/', (req, res) => {
+  let authData = fetch(".auth/me");
   res.send(authData)
 })
 
