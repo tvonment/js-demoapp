c33bc586cc4ba0831651d6503bc551140a870be5
Thomas <65050898+tvonment@users.noreply.github.com>
2022-06-02 09:40:58 +0000

    try .auth/me

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-//const axios = require('axios');
+const axios = require('axios');
 
 app.get('/', async (req, res) => {
-  //let authDataRes = await axios.get('/.auth/me')
-  //res.send(authDataRes.data);
-  res.send("Hello World!");
+  let authDataRes = await axios.get('.auth/me')
+  res.send(authDataRes.data);
 })
 
 app.listen(port, () => {
