0d8bb2109af91ccc377681a9ee8dbdcba4221775
Thomas <65050898+tvonment@users.noreply.github.com>
2022-06-02 08:07:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const axios = require('axios');
+//const axios = require('axios');
 
 app.get('/', async (req, res) => {
-  let authDataRes = await axios.get('/.auth/me')
-  res.send(authDataRes.data);
+  //let authDataRes = await axios.get('/.auth/me')
+  //res.send(authDataRes.data);
+  res.send("Hello World!");
 })
 
 app.listen(port, () => {
